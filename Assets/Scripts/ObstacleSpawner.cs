66fc09e809175d14ec45f6f7b225981b58bb6f92
Balloon208 <gnswldms2@gmail.com>
2024-05-27 16:02:14 +0900

    Add : Repeat Spawn

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     public GameObject obstacle;
+    public float min_delay;
+    public float max_delay;
+    private float delay;
+    private bool summonlock;
 
     int summonline = 0;
-    void Start()
+    void Summon()
     {
         summonline = Random.Range(0, GameManager.Instance.lines.Length);
-        if(obstacle.GetComponent<Obstacle>().summonpos == Obstacle.Summonpos.Left)
+        if (obstacle.GetComponent<Obstacle>().summonpos == Obstacle.Summonpos.Left)
         {
             Instantiate(obstacle, new Vector2(GameManager.Instance.summonbars[0].transform.position.x, GameManager.Instance.lines[summonline].transform.position.y), Quaternion.identity);
         }
@@ -23,6 +27,18 @@ public class ObstacleSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        delay = Random.Range(min_delay, max_delay);
+        if(summonlock == false)
+        {
+            Summon();
+            summonlock = true;
+            StartCoroutine(Wait(delay));
+        }
+    }
+
+    IEnumerator Wait(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        summonlock = false;
     }
 }
