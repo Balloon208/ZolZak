f2e37e22fab9b2ef799ea5cac4467cdce4ad4efe
Balloon208 <gnswldms2@gmail.com>
2024-06-13 23:21:39 +0900

    Add : LobbyScene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,4 +9,73 @@ public class GameManager : Singleton<GameManager>
     public GameObject[] summonbars;
     public GameObject[] deletebars;
     public bool gameover;
+
+    public int[] upgradehpcost =
+    {
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        6000,
+        7000,
+        8000,
+        9000,
+        10000,
+        12000,
+        14000,
+        16000,
+        18000,
+        20000,
+        22000,
+        24000,
+        26000,
+        28000
+    };
+
+    public int[] upgradespeedcost =
+    {
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        6000,
+        7000,
+        8000,
+        9000,
+        10000,
+        12000,
+        14000,
+        16000,
+        18000,
+        20000,
+        22000,
+        24000,
+        26000,
+        28000
+    };
+
+    public int[] upgradeitemcost =
+    {
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        6000,
+        7000,
+        8000,
+        9000,
+        10000,
+        12000,
+        14000,
+        16000,
+        18000,
+        20000,
+        22000,
+        24000,
+        26000,
+        28000
+    };
 }
